efb20dd039011c7995623f6956701d213d2044df
Jonathan Tiede <skysoldiermars@gmail.com>
2018-03-12 15:55:48 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,34 @@
 class Owner
-  # code goes here
+  require 'pry'
+
+  attr_accessor :name
+  attr_writer :species
+
+  @@owners = []
+  @@owner_count = 0
+
+  def initialize(name)
+    @name = name
+    @@owners << self
+  end
+
+  def species(species)
+    @species = species
+    binding.pry
+
+  end
+
+  def self.all
+    @@owners
+  end
+
+  def self.count
+    @@owners.count
+  end
+
+  def self.reset_all
+    @@owners = []
+    @@owners.count
+  end
+
 end
